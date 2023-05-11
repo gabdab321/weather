648b857352785cfbed5ee8c8e14436890c9f2962
Ivan Postryhan <vaniapostrigan106@gmail.com>
2023-05-11 22:19:13 +0300

    made LanguageSwitcher component

---
+++ src/components/LanguageSwitcher/LanguageSwitcher.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import cl from "./LanguageSwitcher.module.scss"
+import uaFlag from "../../assets/flags/ukraineFlag.png"
+import gbFlag from "../../assets/flags/gbFlag.png"
+import {useTranslation} from "react-i18next";
+
+
+/* Switches page language between English and Ukrainian */
+const LanguageSwitcher = () => {
+    const { i18n } = useTranslation()
+
+    /* changes language */
+    function changeLanguage(language: string) {
+        i18n.changeLanguage(language)
+    }
+
+    return (
+        <div className={cl.switcher}>
+            <img onClick={() => changeLanguage("ua")} className={cl.flag} src={uaFlag} alt="ukraine"/>
+            <img onClick={() => changeLanguage("en")} className={cl.flag} src={gbFlag} alt="great britain"/>
+        </div>
+    );
+};
+
+export default LanguageSwitcher;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,21 +2,13 @@ import React from 'react';
 import cl from "./Navbar.module.scss"
 import Search from "../Search/Search";
 import {useTranslation} from "react-i18next";
+import LanguageSwitcher from "../LanguageSwitcher/LanguageSwitcher";
 
 const Navbar = () => {
-    const { i18n } = useTranslation()
-
-    function changeLanguage(language: string) {
-        i18n.changeLanguage(language)
-    }
-
     return (
         <div className={cl.navbar}>
             <Search/>
-            <div>
-                <button onClick={() => changeLanguage("en")}>EN</button>
-                <button onClick={() => changeLanguage("ua")}>UA</button>
-            </div>
+            <LanguageSwitcher/>
         </div>
     );
 };
